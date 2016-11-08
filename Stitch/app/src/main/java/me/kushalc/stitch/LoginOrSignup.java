b78ed3adaea5fe21885f4c4577bc6b22a522506b
Kushal <kcutari@gmail.com>
2016-11-08 15:21:36 -0800

    Setup Activities

---
+++ Stitch/app/src/main/java/me/kushalc/stitch/LoginOrSignup.java
@@ -0,0 +1,13 @@
+package me.kushalc.stitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginOrSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_signup);
+    }
+}
