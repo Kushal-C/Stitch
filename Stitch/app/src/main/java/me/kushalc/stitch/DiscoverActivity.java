7213f26732e3d1dce2e4be5642de7b99fc5f11f4
Kushal <kcutari@gmail.com>
2016-11-10 16:07:43 -0800

    Got custom fonts finished, buttons finished, spacing finished, custom style finished. Need to migrate it to proper activit and need to set LoginOrSignup as the launcher activity

--- Stitch/app/src/main/java/me/kushalc/stitch/DiscoverActivity.java
+++ Stitch/app/src/main/java/me/kushalc/stitch/DiscoverActivity.java
@@ -12,10 +12,12 @@ public class DiscoverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
 
-        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fonts/quicksand_light.otf");
-        Button myTextView = (Button)findViewById(R.id.button_login_one);
-        Button myTextView2 = (Button)findViewById(R.id.button2);
-        myTextView.setTypeface(myTypeface);
-        myTextView2.setTypeface(myTypeface);
+        Typeface quickBold = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.ttf");
+        Button loginButton = (Button)findViewById(R.id.button_login_one);
+        Button signUpButton = (Button)findViewById(R.id.button_signup_one);
+        signUpButton.setTypeface(quickBold);
+        loginButton.setTypeface(quickBold);
+
+
     }
 }
--- Stitch/app/src/main/java/me/kushalc/stitch/LoginOrSignup.java
+++ Stitch/app/src/main/java/me/kushalc/stitch/LoginOrSignup.java
@@ -1,7 +1,9 @@
 package me.kushalc.stitch;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class LoginOrSignup extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginOrSignup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_or_signup);
+
+        Typeface quickBold = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.ttf");
+        Button loginButton = (Button)findViewById(R.id.button_login_one);
+        loginButton.setTypeface(quickBold);
     }
 }
