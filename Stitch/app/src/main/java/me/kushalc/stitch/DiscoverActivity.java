a31acdaf069065d740851bf238602281ad942f2a
Kushal <kcutari@gmail.com>
2016-11-10 18:30:12 -0800

    Launcher Activity properly setup, set on Click Listeners

--- Stitch/app/src/main/java/me/kushalc/stitch/DiscoverActivity.java
+++ Stitch/app/src/main/java/me/kushalc/stitch/DiscoverActivity.java
@@ -1,9 +1,7 @@
 package me.kushalc.stitch;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
 public class DiscoverActivity extends AppCompatActivity {
 
@@ -12,11 +10,7 @@ public class DiscoverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
 
-        Typeface quickBold = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.ttf");
-        Button loginButton = (Button)findViewById(R.id.button_login_one);
-        Button signUpButton = (Button)findViewById(R.id.button_signup_one);
-        signUpButton.setTypeface(quickBold);
-        loginButton.setTypeface(quickBold);
+
 
 
     }
--- Stitch/app/src/main/java/me/kushalc/stitch/LoginOrSignup.java
+++ Stitch/app/src/main/java/me/kushalc/stitch/LoginOrSignup.java
@@ -1,8 +1,10 @@
 package me.kushalc.stitch;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class LoginOrSignup extends AppCompatActivity {
@@ -14,6 +16,20 @@ public class LoginOrSignup extends AppCompatActivity {
 
         Typeface quickBold = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.ttf");
         Button loginButton = (Button)findViewById(R.id.button_login_one);
+        Button signUpButton = (Button)findViewById(R.id.button_signup_one);
+        signUpButton.setTypeface(quickBold);
         loginButton.setTypeface(quickBold);
+
     }
+
+    public void login(View view){
+        Intent i = new Intent(getApplicationContext(), DiscoverActivity.class);
+        startActivity(i);
+    }
+
+    public void signUp(View view){
+        Intent i = new Intent(getApplicationContext(), DiscoverActivity.class);
+        startActivity(i);
+    }
+
 }
