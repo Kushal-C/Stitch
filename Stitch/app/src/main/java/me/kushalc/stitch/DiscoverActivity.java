f02ea3a37fdf2ff1122f4135c30b6bc63488ac3a
Kushal <kcutari@gmail.com>
2016-11-27 18:01:02 -0800

    Finished discover page minus sidebar

--- Stitch/app/src/main/java/me/kushalc/stitch/DiscoverActivity.java
+++ Stitch/app/src/main/java/me/kushalc/stitch/DiscoverActivity.java
@@ -1,5 +1,6 @@
 package me.kushalc.stitch;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -25,13 +26,59 @@ public class DiscoverActivity extends AppCompatActivity
         navBarText.setTypeface(quickBold);
     }
 
+    /*
+    Goes to search activity
+     */
     public void search(View view)
     {
-
+        Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+        i.putExtra("Value","Search");
+        startActivity(i);
     }
 
+    /*
+    Opens navigationBar on the side
+     */
     public void openNavbar(View view)
     {
 
     }
+
+    /*
+    Click from an image button opens feed relating to that item
+     */
+    public void openFeed(View view)
+    {
+        int viewId = view.getId();
+
+        Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+
+        switch (viewId)
+        {
+            case R.id.discover_image_button_accessories:
+                i.putExtra("Value","Accessories");
+                break;
+            case R.id.discover_image_button_dresses:
+                i.putExtra("Value","Dresses");
+                break;
+            case R.id.discover_image_button_featured:
+                i.putExtra("Value","Featured");
+                break;
+            case R.id.discover_image_button_jackets:
+                i.putExtra("Value","Jackets");
+                break;
+            case R.id.discover_image_button_pants:
+                i.putExtra("Value","Pants");
+                break;
+            case R.id.discover_image_button_shirts:
+                i.putExtra("Value","Shirts");
+                break;
+            case R.id.discover_image_button_shoes:
+                i.putExtra("Value","Shoes");
+                break;
+        }
+
+        startActivity(i);
+    }
+
 }
